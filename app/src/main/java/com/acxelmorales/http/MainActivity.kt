5ab416477746a871018385ba7b2ab18c30965f58
Acxel Morales <acxelmorales97@gmail.com>
2021-11-03 10:36:38 -0600

    Added native http handling

--- app/src/main/java/com/acxelmorales/http/MainActivity.kt
+++ app/src/main/java/com/acxelmorales/http/MainActivity.kt
@@ -2,9 +2,16 @@ package com.acxelmorales.http
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.StrictMode
+import android.util.Log
 
 import android.widget.Button
 import android.widget.Toast
+import java.io.IOException
+
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +26,38 @@ class MainActivity : AppCompatActivity() {
             } else {
                 Toast.makeText(this, "No tenemos red", Toast.LENGTH_LONG).show()
             }
+        }
+
+        val btnHttpHandler = findViewById<Button>(R.id.btnHttpHandler)
+        btnHttpHandler.setOnClickListener {
+            if (Network.networkExist(this)) {
+                Log.d("HANDLE", this.downloadData("https://www.google.com/"))
+            } else {
+                Toast.makeText(this, "No tenemos red", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun downloadData(url: String): String {
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+
+        var inputStream: InputStream? = null
+
+        try {
+            val url = URL(url)
+            val connection = url.openConnection() as HttpURLConnection
+
+            connection.requestMethod = "GET"
+            connection.connect()
+
+            inputStream = connection.inputStream
+            return  inputStream.bufferedReader().use {
+                it.readText()
+            }
+        } finally {
+            if (inputStream != null) inputStream?.close()
         }
     }
 
